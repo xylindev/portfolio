f1c33308363a2eb1ce71b6ffb2614831516e164a
Fromagerie 🧀 <elkhair.nourdine.pro@gmail.com>
2025-12-25 12:27:50 +0100

    adjust nav & add portfolio section

--- assets/js/discover/nav.js
+++ assets/js/discover/nav.js
@@ -1,12 +1,28 @@
-const navItems = document.querySelectorAll('#portfolio > nav li ')
-let currentSection = document.querySelector('#portfolio .selected')
+let navItemSelected = document.querySelector('#portfolio .selected')
+let currentSection = document.querySelector('#portfolio #introduction')
+
+function createMapSection(navItems, sections) {
+    const sectionMap = new Map()
+
+    for(let i=0; i<6; i++)
+        sectionMap.set(navItems[i], sections[i])
+    
+    return sectionMap
+}
 
 export const changeSection = () => {
-    navItems.forEach(item => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('selected')
-            currentSection.classList.toggle('selected')
-            currentSection = item
+    const navItems = [...document.querySelectorAll('#portfolio > nav li')]
+    const sections = [...document.querySelectorAll('#portfolio > section')]
+    let sectionMap = createMapSection(navItems, sections)
+
+    for(const [key, value] of sectionMap) {
+        key.addEventListener('click', () => {
+            key.classList.toggle('selected')
+            navItemSelected.classList.toggle('selected')
+            value.classList.toggle('hidden')
+            currentSection.classList.toggle('hidden')
+            navItemSelected = key
+            currentSection = value
         })
-    })
+    }
 }
