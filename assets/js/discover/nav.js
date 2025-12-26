bfd94f62ee96e7ff0da622295736ce60ff1afca0
Fromagerie 🧀 <elkhair.nourdine.pro@gmail.com>
2025-12-26 11:24:54 +0100

    scrolling section

--- assets/js/discover/nav.js
+++ assets/js/discover/nav.js
@@ -1,20 +1,66 @@
 const navItems = document.querySelectorAll('#discover nav a')
 let navItemSelected = document.querySelector('#discover nav .selected')
 
+const navItemsMap = createNavItemsMap()
+
+const selected = 'selected'
+let eventIsClick = false
+let idx = 0
+
+function createNavItemsMap() {
+    const navItemsMap = new Map()
+    
+    for(let i=0; i<navItems.length; i++)
+        navItemsMap.set(navItems[i], i)
+
+    return navItemsMap
+}
+
 export const changeNavSelectedByClick = () => {
-    navItems.forEach(item => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('selected')
-            navItemSelected.classList.toggle('selected')
-            navItemSelected = item
+    for(const [navItem, index] of navItemsMap) {
+        navItem.addEventListener('click', () => {
+            navItem.classList.toggle(selected)
+            navItemSelected.classList.toggle(selected)
+            navItemSelected = navItem
+            idx = index
+            eventIsClick = true
         })
-    })
+    }
 }
 
 export const changeNavSelectedByScroll = () => {
     const portfolio = document.querySelector('#portfolio')
+    const portfolioSection = document.querySelectorAll('#portfolio > section')
+    const bodyHeigth = document.body.getBoundingClientRect().height
+
+    let previousSection = portfolioSection[idx]
+    let nextSection = portfolioSection[idx+1]
 
     portfolio.addEventListener('scroll', () => {
-        
+        let previousSectionBottom = previousSection.getBoundingClientRect().bottom
+        let nextSectionTop = nextSection.getBoundingClientRect().top
+
+        if
+        (
+            !eventIsClick && 
+            idx < (navItems.length-1) && 
+            nextSectionTop < (bodyHeigth/2)
+        ) idx++
+
+        if
+        (
+            !eventIsClick && 
+            idx > 0 && 
+            previousSectionBottom > (bodyHeigth/2)
+        ) idx--
+
+        previousSection = (idx-1 >= 0) ? portfolioSection[idx-1] : portfolioSection[0]
+        nextSection = (idx+1 <= 5) ? portfolioSection[idx+1] : portfolioSection[5]
+
+        navItems[idx].classList.toggle(selected)
+        navItemSelected.classList.toggle(selected)
+        navItemSelected = navItems[idx]
+
+        eventIsClick = false
     })
 }
